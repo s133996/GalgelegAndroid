6861b6a0f086a86bd2487e784b748c8455605340
s133996 <mit.navn.marcus@jubii.dk>
2017-11-12 15:12:29 +0100

    Ny galgelogik

--- app/src/main/java/com/example/marcus/galgeleg/Galgelogik.java
+++ app/src/main/java/com/example/marcus/galgeleg/Galgelogik.java
@@ -1,4 +1,4 @@
-package com.example.marcus.galgeleg;
+package galgeleg;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -10,7 +10,8 @@ import java.util.HashSet;
 import java.util.Random;
 
 public class Galgelogik {
-  private ArrayList<String> muligeOrd = new ArrayList<String>();
+  /** AHT afprøvning er muligeOrd synlig på pakkeniveau */
+  ArrayList<String> muligeOrd = new ArrayList<String>();
   private String ordet;
   private ArrayList<String> brugteBogstaver = new ArrayList<String>();
   private String synligtOrd;
@@ -62,6 +63,8 @@ public class Galgelogik {
     muligeOrd.add("gangsti");
     muligeOrd.add("skovsnegl");
     muligeOrd.add("solsort");
+    muligeOrd.add("seksten");
+    muligeOrd.add("sytten");
     nulstil();
   }
 
@@ -125,6 +128,7 @@ public class Galgelogik {
 
 
   public static String hentUrl(String url) throws IOException {
+    System.out.println("Henter data fra " + url);
     BufferedReader br = new BufferedReader(new InputStreamReader(new URL(url).openStream()));
     StringBuilder sb = new StringBuilder();
     String linje = br.readLine();
@@ -137,19 +141,27 @@ public class Galgelogik {
 
 
   public void hentOrdFraDr() throws Exception {
-    String data = hentUrl("http://dr.dk");
+    String data = hentUrl("https://dr.dk");
     //System.out.println("data = " + data);
 
     data = data.substring(data.indexOf("<body")). // fjern headere
             replaceAll("<.+?>", " ").toLowerCase(). // fjern tags
+            replaceAll("&#198;", "æ"). // erstat HTML-tegn
+            replaceAll("&#230;", "æ"). // erstat HTML-tegn
+            replaceAll("&#216;", "ø"). // erstat HTML-tegn
+            replaceAll("&#248;", "ø"). // erstat HTML-tegn
+            replaceAll("&oslash;", "ø"). // erstat HTML-tegn
+            replaceAll("&#229;", "å"). // erstat HTML-tegn
             replaceAll("[^a-zæøå]", " "). // fjern tegn der ikke er bogstaver
             replaceAll(" [a-zæøå] "," "). // fjern 1-bogstavsord
             replaceAll(" [a-zæøå][a-zæøå] "," "); // fjern 2-bogstavsord
 
     System.out.println("data = " + data);
+    System.out.println("data = " + Arrays.asList(data.split("\\s+")));
     muligeOrd.clear();
     muligeOrd.addAll(new HashSet<String>(Arrays.asList(data.split(" "))));
 
     System.out.println("muligeOrd = " + muligeOrd);
     nulstil();
-  }}
+  }
+}
